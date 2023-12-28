ff62c1a5d0c76cdb76fa9f334e663ca84470e11b
Nguyen Thanh Bac <bacdzno1@gmail.com>
2023-12-28 08:51:32 +0700

    FE

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Sepolia } from "@thirdweb-dev/chains";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider
+    activeChain={Sepolia}
+    clientId="1ea33c1b2f6687e5de3f02086d0f89e2"
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
